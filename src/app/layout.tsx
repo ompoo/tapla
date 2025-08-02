362d8db0ee1e415f98d06b31827c3e36db989bea
yufoxda <147848207+yufoxda@users.noreply.github.com>
2025-08-02 11:50:13 +0900

    feat:テーブルcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/component/header";
+import Header from "@/component/header/header";
+import Footer from "@/component/footer/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
