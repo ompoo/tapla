38f482f6865c13ec77ff1b2324c353f7640e9362
yufoxda <147848207+yufoxda@users.noreply.github.com>
2025-08-13 15:03:23 +0900

    feat:ホーム画面の見た目

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,48 @@
 import Link from "next/link";
 import styles from "./page.module.css";
+import BackgroundPattern from "@/component/BackGroundPattern";
 
 export default async function Home() {
   return (
-    <div >
-      <h2 className={styles.subtitle}>日程調整サイト</h2>
-      <h1 className={styles.title}>tapla</h1>
+    <>
+      <BackgroundPattern />
+      <div className={styles.heroTitle}>
+        <h1 className={styles.title}>tapla</h1>
+        <p>タップしてみんなの予定を教えろください</p>
+        <Link href="/create">
+          <button className={styles.eventCreateButton}>
+            try
+          </button>
+        </Link>
+      </div>
+
+      <h2>使い方</h2>
+      <p>3ステップで簡単に予定調整</p>
+
+      <ol>
+        <li>予定を作成</li>
+        <li>タップで参加</li>
+        <li>結果を確認</li>
+      </ol>
+
+      <h2>主な特徴</h2>
+
+      <ol>
+        <li>認証機能</li>
+        <li>自動入力</li>
+        <li>モバイル対応</li>
+      </ol>
+
+      <h2>今すぐサークルの予定調整を楽にしましょう！</h2>
+      <p>面倒な予定調整はもう終わり。taplaで、みんなが笑顔になる時間を作りましょう。</p>
+
       <Link href="/create">
         <button className={styles.eventCreateButton}>
-          イベントを作成
+          無料で始める
         </button>
       </Link>
-    
-    </div>
+
+      
+    </>
   );
 }
