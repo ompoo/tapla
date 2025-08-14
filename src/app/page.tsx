de5eb40900867f0248d47695a2bb430f97633400
yufoxda <147848207+yufoxda@users.noreply.github.com>
2025-08-14 16:24:03 +0900

    move:backGroundファイル

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import styles from "./page.module.css";
-import BackgroundPattern from "@/app/_component/BackGroundPattern";
+import BackgroundPattern from "@/BackGroundPattern";
 import  HowToUse from "@/app/_component/HowToUse";
 import Feature from "./_component/Feature";
 
